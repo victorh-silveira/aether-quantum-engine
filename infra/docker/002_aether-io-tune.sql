c37ab42620d94627cf099d0d5398b3392151520f
Victor Hugo Carvalho Silveira <victorhcs_silveira@hotmail.com>
2026-09-02 10:23:25 -0300

    perf(infra): otimizar infraestrutura de dados e microservicos ml
    - Configura loop e interpretador de alto desempenho para microservicos
    - Ativa variaveis de ambiente de execucao rapida em containers
    - Otimiza configuracao do redis com fila estendida e desalocacao assincrona
    - Calibra banco relacional com buffers dedicados para disco solido

--- infra/docker/002_aether-io-tune.sql
+++ infra/docker/002_aether-io-tune.sql
@@ -1,3 +1,10 @@
 ALTER SYSTEM SET checkpoint_completion_target = 0.95;
 ALTER SYSTEM SET max_wal_size = '2GB';
+ALTER SYSTEM SET shared_buffers = '256MB';
+ALTER SYSTEM SET work_mem = '16MB';
+ALTER SYSTEM SET maintenance_work_mem = '64MB';
+ALTER SYSTEM SET effective_cache_size = '768MB';
+ALTER SYSTEM SET random_page_cost = 1.1;
+ALTER SYSTEM SET effective_io_concurrency = 200;
+ALTER SYSTEM SET max_parallel_workers_per_gather = 2;
 SELECT pg_reload_conf();
